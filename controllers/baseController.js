a01ce32a31962a62d1c6615bc823344567063f70
rfmarves <rfmarves@gmail.com>
2025-09-22 19:55:41 -0600

    completed w4 Sessions and messages

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -3,6 +3,7 @@ const baseController = {}
 
 baseController.buildHome = async function(req, res){
   const nav = await utilities.getNav()
+  // req.flash("error", "This is a flash message")
   res.render("index", {title: "Home", nav})
 }
 
